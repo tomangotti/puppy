1f7c4a69ba03418d1c257191edca2c55d967d45a
thomas <tom.angotti11@gmail.com>
2023-07-21 11:06:49 -0600

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tomangotti/puppy
 
 go 1.20
+
+require github.com/tomangotti/dog v0.0.0-20230721165616-fddf37b9f944 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,19 @@
 package puppy
 
+import (
+	"github.com/tomangotti/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	s1:= dog.WhenGrownUps(Bark())
+
+	return s1
 }
